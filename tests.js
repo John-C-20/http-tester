f1f62b8fd231b89c60f3acf0eb655cee2fcec0a5
John Cheung <johncheung1997@gmail.com>
2021-12-22 19:03:28 -0500

    initial commit

---
+++ tests.js
@@ -0,0 +1,19 @@
+// delete this 
+const http = require('http')
+const data = JSON.stringify({
+    msg: "hi"
+})
+const options = {
+    hostname: 'localhost',
+    port: 1997,
+    path: "/",
+    method: 'POST'
+}
+const req = http.request(options, res => {
+    res.on('data', d => console.log(d))
+}) 
+
+req.on('error', error => console.log(error))
+req.write(data)
+req.end()
+//
